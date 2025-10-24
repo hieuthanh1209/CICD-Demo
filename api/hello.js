8bc9d2593d2804239aee75bd131be453c47a014b
Hieu Vo Nguyen Thanh <thanhhieu5100@gmail.com>
2025-10-24 08:49:43 +0700

    ERROR COMMIT

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   return res.status(200).json({
-    message: "Hello from Vercel, tôi là Hiếu!",
+    message: "Hello from Vercel, tôi là Hiếu!,
     time: new Date().toISOString(),
   });
 }
